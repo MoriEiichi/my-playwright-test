ef6b73e32caed56f3a2a94ed5ca44e7201ce6895
Eiichi <mori@morieiichi.com>
2025-06-09 11:57:57 +0900

    body-parserの使用を廃止し、express.urlencoded()に置き換え

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
 // フォームデータの解析
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // 静的ファイルの提供（publicフォルダに login.html や dashboard.html など）
 app.use(express.static(path.join(__dirname, 'public')));
